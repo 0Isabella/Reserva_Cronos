c13aaa8966f84533ceb395a5bf2ec3ae0f98795e
Lisa <0isamatos0@gmail.com>
2025-11-11 09:24:58 -0300

    Adicionar o cliente

---
+++ Models/Clientes.cs
@@ -0,0 +1,9 @@
+namespace CronosReserva.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public int Telefone {get; set; }
+    }
+}
--- Models/Reservas.cs
+++ Models/Reservas.cs
@@ -1,13 +1,13 @@
 namespace CronosReserva.Models
 {
     public class Reserva
-        {
-            public int Id { get; set; }
-            public string DataInicio { get; set; }
-            public string DataFim { get; set; } 
-            public float Tamanho { get; set; }
-            public string NomeLocal { get; set; }
-            public float Valor{ get; set; } 
-            public string Descricao { get; set; }
-        }
+    {
+        public int Id { get; set; }
+        public string DataInicio { get; set; }
+        public string DataFim { get; set; }
+        public float Tamanho { get; set; }
+        public string NomeLocal { get; set; }
+        public float Valor { get; set; }
+        public string Descricao { get; set; }
+    }
 }
